1d1cb5b0704cea5a3063b771550ff7c17d17a1b5
Kanishk <knshksnghl@gmail.com>
2021-07-16 14:03:44 +0530

    Code optimization, converted everything to pug, some improvement in sass files, added separate projects page, debugged the need of server restart everytime data in gsheet changes

--- app.js
+++ app.js
@@ -22,14 +22,77 @@ app.use(express.urlencoded())
 app.set('view engine','pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// setInterval(()=>{
-	// Authorizing client for fetching data from google sheets
-	const client = new google.auth.JWT(
-		keys.client_email,
-		null,
-		keys.private_key,
-		['https://www.googleapis.com/auth/spreadsheets']
-	);
+
+// Authorizing client for fetching data from google sheets
+const client = new google.auth.JWT(
+	keys.client_email,
+	null,
+	keys.private_key,
+	['https://www.googleapis.com/auth/spreadsheets']
+);
+
+client.authorize(function(err, tokens){
+	if(err){
+		console.log('There is an error' + err);
+		return;
+	}else{
+		console.log('connected');
+		gsrun(client)
+	}
+});
+
+// Active Member data from google sheets
+async function gsrun(cl){	
+	const gsapi = google.sheets({version: 'v4', auth: cl });
+	
+	const opt = {
+		spreadsheetId: '1eTZLl7o5DUWmrSl6oHLL_OtHjMjV9eCi2lb2N7UElMw',
+		range: 'Data!A2:C'
+	};
+	
+	let data = await gsapi.spreadsheets.values.get(opt);
+	let dtarr = data.data.values;
+	
+	dtarr = dtarr.map(function(r){
+		while(r.length < 2){
+			r.push('');
+		}
+		return r;
+	});
+	
+	newdt = dtarr.sort(function(a,b){
+		return b[2]-a[2];
+	});
+}
+
+
+// ENDPOINTS
+
+// Home Page
+app.get('/', (req,res) =>{
+	res.status(200).render('./index', {title: "Home | Metis"});
+});
+
+// Projects Page
+app.get('/projects', (req,res) =>{
+	res.status(200).render('projects', {title: "Projects | Metis"});
+});
+
+
+// Resources
+app.get('/resources', (req,res) =>{
+	request(`${STRAPI_API_URL}/cards`, function(error, response, body){
+		
+		if(!error && response.statusCode == 200){
+			var parseBody = JSON.parse(body);
+			res.status(200).render('resources.pug', {title: "Resources | Metis", content: parseBody, strapiurl: STRAPI_API_URL});
+		}
+	})
+});
+
+
+// Active Members page
+app.get('/actmembers', (req,res) =>{
 
 	client.authorize(function(err, tokens){
 		if(err){
@@ -41,69 +104,20 @@ app.set('views', path.join(__dirname, 'views'));
 		}
 	});
 
-	// Active Member data from google sheets
-	async function gsrun(cl){	
-		const gsapi = google.sheets({version: 'v4', auth: cl });
-		
-		const opt = {
-			spreadsheetId: '1eTZLl7o5DUWmrSl6oHLL_OtHjMjV9eCi2lb2N7UElMw',
-			range: 'Data!A2:C'
-		};
-		
-		let data = await gsapi.spreadsheets.values.get(opt);
-		let dtarr = data.data.values;
-		
-		dtarr = dtarr.map(function(r){
-			while(r.length < 2){
-				r.push('');
-			}
-			return r;
-		});
-		
-		newdt = dtarr.sort(function(a,b){
-			return b[2]-a[2];
-		});
-	}
-// }, 5000);
-
-
-
-// ENDPOINTS
-
-// Home Page
-app.get('/', (req,res) =>{
-	res.status(200).render('../index');
-});
-
-
-// Resources
-app.get('/resources', (req,res) =>{
-	request(`${STRAPI_API_URL}/cards`, function(error, response, body){
-		
-		if(!error && response.statusCode == 200){
-			var parseBody = JSON.parse(body);
-			res.status(200).render('resources.pug', {content: parseBody, strapiurl: STRAPI_API_URL});
-		}
-	})
-});
-
-
-// Active Members page
-app.get('/actmembers', (req,res) =>{
 	// console.log(newdt);
-	res.status(200).render('activemem.pug', {actdt: newdt.slice(0,10)});
+	res.status(200).render('activemem.pug', {title: "Active Members | Metis", actdt: newdt.slice(0,10)});
 		
 		
-		// To update data in sheet
-		// const updtopt = {
-			// 	spreadsheetId: '1eTZLl7o5DUWmrSl6oHLL_OtHjMjV9eCi2lb2N7UElMw',
-			// 	range: 'Data!N2',
-			// 	valueInputOption: 'USER_ENTERED',
-			// 	resource: { values: newdt.slice(0,10) }
-		// };
-	
-		// let res = await gsapi.spreadsheets.values.update(updtopt);
-		// console.log(res);
+	// To update data in sheet
+	// const updtopt = {
+		// 	spreadsheetId: '1eTZLl7o5DUWmrSl6oHLL_OtHjMjV9eCi2lb2N7UElMw',
+		// 	range: 'Data!N2',
+		// 	valueInputOption: 'USER_ENTERED',
+		// 	resource: { values: newdt.slice(0,10) }
+	// };
+
+	// let res = await gsapi.spreadsheets.values.update(updtopt);
+	// console.log(res);
 	
 });
 
